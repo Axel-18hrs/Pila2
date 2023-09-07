edc019406d620589c22af08f5bb64238a0c46e13
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-09-06 21:06:54 -0600

    Agregar archivos de proyecto.

---
+++ Pila2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pila2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> pila = new Stack<int>();
+            bool salir = true;
+
+            while (salir)
+            {
+                Console.WriteLine("// Select an option" +
+                    "\n[1]: Enter a value " +
+                    "\n[2]: Delete a value" +
+                    "\n[3]: Show stack" +
+                    "\n[4]: Exit");
+
+                int opcion;
+
+                try
+                {
+                    opcion = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                switch (opcion)
+                {
+
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Enter a value:");
+                        pila.Push(Convert.ToInt32(Console.ReadLine()));
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Number to search for:");
+                        if (pila.Count > 0)
+                        {
+                            int elementoDesapilado = pila.Pop();
+                            Console.Clear();
+                            Console.WriteLine("// Element popped from the stack: " + elementoDesapilado);
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("The stack is empty");
+                        }
+
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        foreach(int i in pila)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        break;
+
+                    case 4:
+                        salir = false;
+                        Console.Clear();
+                        Console.WriteLine("Good bye <3");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
